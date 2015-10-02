5a2ebc03af3808d764635614b01108aae3d934e9
Lauren Granger <lr.granger21@gmail.com>
2015-10-02 10:52:41 -0700

    NyanCat class

--- draw_cat.rb
+++ draw_cat.rb
@@ -1,15 +1,572 @@
+require "colorize"
+class NyanCat
 
-puts "  +      o     +              o    "
-puts "    +             o     +       +  "
-puts "o          +                       "
-puts "    o  +           +        +      "
-puts "+        o     o       +        o  "
-puts "-_-_-_-_-_-_-_,------,      o      "
-puts "_-_-_-_-_-_-_-|   /\\_/\\          "
-puts "-_-_-_-_-_-_-~|__( ^ .^)  +     +  "
-puts "_-_-_-_-_-_-_-```  ```             "
-puts "+      o         o   +       o     "
-puts "    +         +                    "
-puts "o        o         o      o     +  "
-puts "    o           +                  "
-puts "+      +     o        o      +     "
+def initialize
+  @cat_state = 6
+end
+
+def cat_six
+print "  +".cyan
+print"      o".magenta
+print"     +".cyan
+print"              o".magenta
+print"    \n"
+print "    +".cyan
+print"             o".magenta
+print"     +".cyan
+print"       +".cyan
+print"  \n"
+print "o".magenta
+print"          +".cyan
+print"                       \n"
+print "    o".magenta
+print"  +".cyan
+print"           +".cyan
+print"        +".cyan
+print"      \n"
+print "+".cyan
+print"        o".magenta
+print"     o".magenta
+print"       +".cyan
+print"        o".magenta
+print"  \n"
+print "-_-_-_-_-_-_-_".red
+print",------,".light_black
+print"      o".magenta
+print"      \n"
+print "_-_-_-_-_-_-_-".yellow
+print"|   /\\_/\\          \n".light_black
+print "-_-_-_-_-_-_-".green
+print"~|__( ^ .^)".light_black
+print"  +".cyan
+print"     +".cyan
+print"  \n"
+print "_-_-_-_-_-_-_-".blue
+print"```  ```             \n".light_black
+print "+".cyan
+print"      o".magenta
+print"         o".magenta
+print"   +".cyan
+print"       o".magenta
+print"     \n"
+print "    +".cyan
+print"         +".cyan
+print"                    \n"
+print "o".magenta
+print"        o".magenta
+print"         o".magenta
+print"      o".magenta
+print"     +".cyan
+print"  \n"
+print "    o".magenta
+print"           +".cyan
+print"                  \n"
+print "+".cyan
+print"      +".cyan
+print"     o".magenta
+print"        o".magenta
+print"      +".cyan
+print"     \n"
+end
+
+def cat_five
+print "  +".black
+print"      o".magenta
+print"     +".black
+print"              o".magenta
+print"    \n"
+print "    +".black
+print"             o".magenta
+print"     +".black
+print"       +".black
+print"  \n"
+print "o".magenta
+print"          +".black
+print"                       \n"
+print "    o".magenta
+print"  +".black
+print"           +".black
+print"        +".black
+print"      \n"
+print "+".black
+print"        o".magenta
+print"     o".magenta
+print"       +".black
+print"        o".magenta
+print"  \n"
+print "-_-_-_-_-_-_-_".red
+print",------,".light_black
+print"      o".magenta
+print"      \n"
+print "_-_-_-_-_-_-_-".yellow
+print"|   /\\_/\\          \n".light_black
+print "-_-_-_-_-_-_-".green
+print"~|__( ^ .^)".light_black
+print"  +".black
+print"     +".black
+print"  \n"
+print "_-_-_-_-_-_-_-".blue
+print"```  ```             \n".light_black
+print "+".black
+print"      o".magenta
+print"         o".magenta
+print"   +".black
+print"       o".magenta
+print"     \n"
+print "    +".black
+print"         +".black
+print"                    \n"
+print "o".magenta
+print"        o".magenta
+print"         o".magenta
+print"      o".magenta
+print"     +".black
+print"  \n"
+print "    o".magenta
+print"           +".black
+print"                  \n"
+print "+".black
+print"      +".black
+print"     o".magenta
+print"        o".magenta
+print"      +".black
+print"     \n"
+end
+
+def cat_four
+print "  +".black
+print"      o".black
+print"     +".black
+print"              o".black
+print"    \n"
+print "    +".black
+print"             o".black
+print"     +".black
+print"       +".black
+print"  \n"
+print "o".black
+print"          +".black
+print"                       \n"
+print "    o".black
+print"  +".black
+print"           +".black
+print"        +".black
+print"      \n"
+print "+".black
+print"        o".black
+print"     o".black
+print"       +".black
+print"        o".black
+print"  \n"
+print "-_-_-_-_-_-_-_".red
+print",------,".light_black
+print"      o".black
+print"      \n"
+print "_-_-_-_-_-_-_-".yellow
+print"|   /\\_/\\          \n".light_black
+print "-_-_-_-_-_-_-".green
+print"~|__( ^ .^)".light_black
+print"  +".black
+print"     +".black
+print"  \n"
+print "_-_-_-_-_-_-_-".blue
+print"```  ```             \n".light_black
+print "+".black
+print"      o".black
+print"         o".black
+print"   +".black
+print"       o".black
+print"     \n"
+print "    +".black
+print"         +".black
+print"                    \n"
+print "o".black
+print"        o".black
+print"         o".black
+print"      o".black
+print"     +".black
+print"  \n"
+print "    o".black
+print"           +".black
+print"                  \n"
+print "+".black
+print"      +".black
+print"     o".black
+print"        o".black
+print"      +".black
+print"     \n"
+end
+
+def cat_three
+print "  +".black
+print"      o".black
+print"     +".black
+print"              o".black
+print"    \n"
+print "    +".black
+print"             o".black
+print"     +".black
+print"       +".black
+print"  \n"
+print "o".black
+print"          +".black
+print"                       \n"
+print "    o".black
+print"  +".black
+print"           +".black
+print"        +".black
+print"      \n"
+print "+".black
+print"        o".black
+print"     o".black
+print"       +".black
+print"        o".black
+print"  \n"
+print "-_-_-_-_-_-_-_".black
+print",------,".light_black
+print"      o".black
+print"      \n"
+print "_-_-_-_-_-_-_-".yellow
+print"|   /\\_/\\          \n".light_black
+print "-_-_-_-_-_-_-".green
+print"~|__( ^ .^)".light_black
+print"  +".black
+print"     +".black
+print"  \n"
+print "_-_-_-_-_-_-_-".blue
+print"```  ```             \n".light_black
+print "+".black
+print"      o".black
+print"         o".black
+print"   +".black
+print"       o".black
+print"     \n"
+print "    +".black
+print"         +".black
+print"                    \n"
+print "o".black
+print"        o".black
+print"         o".black
+print"      o".black
+print"     +".black
+print"  \n"
+print "    o".black
+print"           +".black
+print"                  \n"
+print "+".black
+print"      +".black
+print"     o".black
+print"        o".black
+print"      +".black
+print"     \n"
+end
+
+def cat_two
+print "  +".black
+print"      o".black
+print"     +".black
+print"              o".black
+print"    \n"
+print "    +".black
+print"             o".black
+print"     +".black
+print"       +".black
+print"  \n"
+print "o".black
+print"          +".black
+print"                       \n"
+print "    o".black
+print"  +".black
+print"           +".black
+print"        +".black
+print"      \n"
+print "+".black
+print"        o".black
+print"     o".black
+print"       +".black
+print"        o".black
+print"  \n"
+print "-_-_-_-_-_-_-_".black
+print",------,".light_black
+print"      o".black
+print"      \n"
+print "_-_-_-_-_-_-_-".black
+print"|   /\\_/\\          \n".light_black
+print "-_-_-_-_-_-_-".green
+print"~|__( ^ .^)".light_black
+print"  +".black
+print"     +".black
+print"  \n"
+print "_-_-_-_-_-_-_-".blue
+print"```  ```             \n".light_black
+print "+".black
+print"      o".black
+print"         o".black
+print"   +".black
+print"       o".black
+print"     \n"
+print "    +".black
+print"         +".black
+print"                    \n"
+print "o".black
+print"        o".black
+print"         o".black
+print"      o".black
+print"     +".black
+print"  \n"
+print "    o".black
+print"           +".black
+print"                  \n"
+print "+".black
+print"      +".black
+print"     o".black
+print"        o".black
+print"      +".black
+print"     \n"
+end
+
+def cat_two
+print "  +".black
+print"      o".black
+print"     +".black
+print"              o".black
+print"    \n"
+print "    +".black
+print"             o".black
+print"     +".black
+print"       +".black
+print"  \n"
+print "o".black
+print"          +".black
+print"                       \n"
+print "    o".black
+print"  +".black
+print"           +".black
+print"        +".black
+print"      \n"
+print "+".black
+print"        o".black
+print"     o".black
+print"       +".black
+print"        o".black
+print"  \n"
+print "-_-_-_-_-_-_-_".black
+print",------,".light_black
+print"      o".black
+print"      \n"
+print "_-_-_-_-_-_-_-".black
+print"|   /\\_/\\          \n".light_black
+print "-_-_-_-_-_-_-".black
+print"~|__( ^ .^)".light_black
+print"  +".black
+print"     +".black
+print"  \n"
+print "_-_-_-_-_-_-_-".blue
+print"```  ```             \n".light_black
+print "+".black
+print"      o".black
+print"         o".black
+print"   +".black
+print"       o".black
+print"     \n"
+print "    +".black
+print"         +".black
+print"                    \n"
+print "o".black
+print"        o".black
+print"         o".black
+print"      o".black
+print"     +".black
+print"  \n"
+print "    o".black
+print"           +".black
+print"                  \n"
+print "+".black
+print"      +".black
+print"     o".black
+print"        o".black
+print"      +".black
+print"     \n"
+end
+
+def cat_one
+print "  +".black
+print"      o".black
+print"     +".black
+print"              o".black
+print"    \n"
+print "    +".black
+print"             o".black
+print"     +".black
+print"       +".black
+print"  \n"
+print "o".black
+print"          +".black
+print"                       \n"
+print "    o".black
+print"  +".black
+print"           +".black
+print"        +".black
+print"      \n"
+print "+".black
+print"        o".black
+print"     o".black
+print"       +".black
+print"        o".black
+print"  \n"
+print "-_-_-_-_-_-_-_".black
+print",------,".light_black
+print"      o".black
+print"      \n"
+print "_-_-_-_-_-_-_-".black
+print"|   /\\_/\\          \n".light_black
+print "-_-_-_-_-_-_-".black
+print"~|__( ^ .^)".light_black
+print"  +".black
+print"     +".black
+print"  \n"
+print "_-_-_-_-_-_-_-".black
+print"```  ```             \n".light_black
+print "+".black
+print"      o".black
+print"         o".black
+print"   +".black
+print"       o".black
+print"     \n"
+print "    +".black
+print"         +".black
+print"                    \n"
+print "o".black
+print"        o".black
+print"         o".black
+print"      o".black
+print"     +".black
+print"  \n"
+print "    o".black
+print"           +".black
+print"                  \n"
+print "+".black
+print"      +".black
+print"     o".black
+print"        o".black
+print"      +".black
+print"     \n"
+end
+
+def cat_dead
+print "  +".cyan
+print"      o".magenta
+print"     +".cyan
+print"              o".magenta
+print"    \n"
+print "    +".cyan
+print"             o".magenta
+print"     +".cyan
+print"       +".cyan
+print"  \n"
+print "o".magenta
+print"          +".cyan
+print"                       \n"
+print "    o".magenta
+print"  +".cyan
+print"           +".cyan
+print"        +".cyan
+print"      \n"
+print "+".cyan
+print"        o".magenta
+print"     o".magenta
+print"       +".cyan
+print"        o".magenta
+print"  \n"
+print "YOU LOOSE           \n".light_black
+print "+".cyan
+print"      o".magenta
+print"         o".magenta
+print"   +".cyan
+print"       o".magenta
+print"     \n"
+print "    +".cyan
+print"         +".cyan
+print"                    \n"
+print "o".magenta
+print"        o".magenta
+print"         o".magenta
+print"      o".magenta
+print"     +".cyan
+print"  \n"
+print "    o".magenta
+print"           +".cyan
+print"                  \n"
+print "+".cyan
+print"      +".cyan
+print"     o".magenta
+print"        o".magenta
+print"      +".cyan
+print"     \n"
+end
+
+def cat_win
+print "  +".cyan.blink
+print"      o".magenta.blink
+print"     +".cyan.blink
+print"              o".magenta.blink
+print"    \n".blink
+print "    +".cyan.blink
+print"             o".magenta.blink
+print"     +".cyan.blink
+print"       +".cyan.blink
+print"  \n".blink
+print "o".magenta.blink
+print"          +".cyan.blink
+print"                       \n".blink
+print "    o".magenta.blink
+print"  +".cyan.blink
+print"           +".cyan.blink
+print"        +".cyan.blink
+print"      \n".blink
+print "+".cyan.blink
+print"        o".magenta.blink
+print"     o".magenta.blink
+print"       +".cyan.blink
+print"        o".magenta.blink
+print"  \n".blink
+print "-_-_-_-_-_-_-_".red.blink
+print",------,".light_black.blink
+print"      o".magenta.blink
+print"      \n".blink
+print "_-_-_-_-_-_-_-".yellow.blink
+print"|   /\\_/\\          \n".light_black.blink
+print "-_-_-_-_-_-_-".green.blink
+print"~|__( ^ .^)".light_black.blink
+print"  +".cyan.blink
+print"     +".cyan.blink
+print"  \n".blink
+print "_-_-_-_-_-_-_-".blue.blink
+print"```  ```             \n".light_black.blink
+print "+".cyan.blink
+print"      o".magenta.blink
+print"         o".magenta.blink
+print"   +".cyan.blink
+print"       o".magenta.blink
+print"     \n".blink
+print "    +".cyan.blink
+print"         +".cyan.blink
+print"                    \n".blink
+print "o".magenta.blink
+print"        o".magenta.blink.blink
+print"         o".magenta.blink
+print"      o".magenta.blink
+print"     +".cyan.blink
+print"  \n".blink
+print "    o".magenta.blink
+print"           +".cyan.blink
+print"                  \n".blink
+print "+".cyan.blink
+print"      +".cyan.blink
+print"     o".magenta.blink
+print"        o".magenta.blink
+print"      +".cyan.blink
+print"     \n".blink
+end
+
+end

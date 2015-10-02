0968791e003879759426750ce099113e36202096
noglows <jessica.noglows@outlook.com>
2015-10-02 09:49:27 -0700

    Working random word generator

--- random_word_gen.rb
+++ random_word_gen.rb
@@ -1,1 +1,7 @@
-random_words = ["kitten", "litter", "catnip", "mouse", "mice", "tuna", "birds"]
+
+
+def random_word_gen
+  random_words = ["kitten", "litter", "catnip", "mouse", "mice", "tuna", "birds"]
+  random_word = random_words[rand(0...random_words.length)]
+  puts "This is a random word: #{random_word}"
+end
